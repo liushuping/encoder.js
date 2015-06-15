2c8ef0b54123f1bb90341f02a9fdef736a030dd7
Shuping <lsp.lhh@gmail.com>
2015-06-15 23:57:34 +0800

    add more to be place holders for encoding methods

--- encoder.js
+++ encoder.js
@@ -41,24 +41,13 @@ Encoder.prototype.encodeHTMLAttr = function(str) {
     return encoded;
 };
 
-Encoder.prototype.decodeHTMLAttr = function(str) {
-    var decoded = '';
-
-    if (str === undefined || str.length === 0) {
-        return decoded;
-    }
-
-    decoded = str.replace(/&amp;/g, '&');
-    decoded = decoded.replace(/&lt;/g, '<');
-    decoded = decoded.replace(/&#39;/g, '\'');
-    decoded = decoded.replace(/&quot;/g, '"');
-
-    return decoded;
-}
-
 Encoder.prototype.encodeJavaScript = function(str) {
     //TODO
 };
+
+Encoder.prototype.decodeJavaScript = function(str) {
+    //TODO
+}
 
 Encoder.prototype.encodeURI = function(str) {
     return encodeURI(str);
@@ -90,6 +79,30 @@ Encoder.prototype.encodeJSON = function(str) {
 
 Encoder.prototype.decodeJSON = function(str) {
     //TODO
+};
+
+Encoder.prototype.percentageEncode = function(str) {
+    //TODO;
+};
+
+Encoder.prototype.percentageDecode = function(str) {
+    //TODO
+};
+
+Encoder.prototype.quotedEncode = function(str) {
+    //TODO
+};
+
+Encoder.prototype.quotedDecode = function(str) {
+    //TODO
+};
+
+Encoder.prototype.base64Encode = function(str) {
+
+};
+
+Encoder.prototype.base64Decode = function(str) {
+
 };
 
 module.exports = new Encoder();
